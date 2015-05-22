f34ffaa0931312e281b254837cff80f708906cab
henrik <geijstedt@telia.com>
2015-05-22 18:54:21 +0200

    Removed ios client since it moved directory.

--- LampMaster/LampMaster/AppConfiguration.swift
+++
@@ -1,9 +0,0 @@
-//
-//  AppConfiguration.swift
-//  LampMaster
-//
-//  Created by Henrik Geijstedt on 05/05/15.
-//  Copyright (c) 2015 Henrik Geijstedt. All rights reserved.
-//
-
-import Foundation
